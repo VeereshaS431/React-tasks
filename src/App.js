f77ddf0232a9524cf6f841cea32c8e14e18baae5
Veeresha S <114753819+VeereshaS431@users.noreply.github.com>
2024-03-27 21:25:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 
 import './App.css';
-import Tables from './functionComponents/tables';
-import Forms from './functionComponents/forms';
-import Lists from './functionComponents/list';
-import CostumImage from './functionComponents/imgg';
 
-import CustomForm from './classComponents/formClass';
-import CustomList from './classComponents/listClass';
-import CustomTables from './classComponents/tableclass';
-import Customimg from './classComponents/imggClass';
 
 function App() {
   return (
     <div className="App">
-      <div className='function'>
-        <h1>Function Components</h1>
-        <CostumImage />
-        <Tables />
-        <Forms />
-        <Lists />
-      </div>
-
-      <div className='classComp'>
-        <h1>Class Components</h1>
-        <Customimg></Customimg>
-        <CustomTables></CustomTables>
-        < CustomForm></CustomForm>
-        <CustomList></CustomList>
-      </div>
+     
     </div>
   );
 }
