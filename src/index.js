a0fc483202c105b984e602e798f2ec876205a0e8
Durga Prasad <dporange.siripuram007@gmail.com>
2024-03-28 11:59:50 +0530

    Login changes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import SignUp from './functionComponents/SignUpComponent';
+import  LoginComponent from './functionComponents/login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <SignUp/>
+    <LoginComponent/>
   </React.StrictMode>
 );
 
